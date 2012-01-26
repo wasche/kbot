27b9ec310742914eb9f2e0972836487cf12d9336
Wil Asche <wil@asche.us>
2012-01-25 22:44:22 -0500

    move setting to config file

---
+++ config.sample.js
@@ -0,0 +1,16 @@
+
+exports.host = 'irc.example.com';
+
+exports.port = 6667;
+
+export.secure = false;
+
+exports.name = 'kbot';
+
+exports.channels = [];
+
+exports.plugins = {
+  admin: {
+    nick: ['admin']
+  }
+};
--- kbot.js
+++ kbot.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 
 var bot = require('ircbot')
+  , merge = require('object-merge').merge
+  , options = merge({
+      host: 'irc.example.com'
+    , port: 6667
+    , secure: false
+    , name: 'kbot'
+    , channels: []
+    , plugins: {}
+    }, require('./config'))
   , kbot
   , r
   ;
@@ -9,15 +18,15 @@ process.on('uncaughtException', function(err) {
   console.log('Uncaught Exception: ' + err);
 });
 
-kbot = new bot('irc.tripadvisor.com', 'kbot', {
-  port: 6667
-, userName: 'kbot'
-, realName: 'kbot'
-, debug: true
-, secure: false
-, channels: ['#social']
+kbot = new bot(options.host, options.name, {
+  port: options.port
+, userName: options.name
+, realName: options.name
+, debug: false
+, secure: options.secure
+, channels: options.channel
 });
 
-//kbot.loadPlugin('admin', {nick: ['wasche']});
-kbot.loadPlugin('bugzilla');
-kbot.loadPlugin('reviewboard');
+for (plugin in options.plugins) {
+  kbot.loadPlugin(plugin, options.plugins[plugin]);
+}
