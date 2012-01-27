e94bea291b1c5c6d5d98621e9f7ebe11c1004fa5
Wil Asche <wil@asche.us>
2012-01-27 14:39:52 -0500

    add trac plugin

---
+++ plugins/trac/cmd.js
@@ -0,0 +1,41 @@
+
+var nodeio = require('node.io')
+  , util = require('util')
+  , format = util.format
+  ;
+
+exports.run = function(info) {
+  var cmd = cmds[info.cmdstr];
+  if (cmd) {
+    cmd(info);
+  }
+};
+
+var cmds = {
+  rb: function(info) {
+    var id = info.rest;
+
+    nodeio.scrape(function() {
+      this.get('http://' + info.plugin.options.host + '/ticket/' + rest + '?format=csv', function(err, data) {
+        if (err) {
+          info.bot.respond(info, "Sorry, I can't find what you're looking for.");
+          return;
+        }
+
+        var lines = data.split('\n');
+        for (var line, i = 1/* skip header line */, l = lines.length; i < l; i++) {
+          // id, summary, reporter, owner, description, type, status, priority, milestone, component, severity, resolution, keywords, cc, loe, verify_trunk, verify_path, verify_pre, verify_live, code_review
+          line = this.parseValues(lines[i]);
+
+          info.bot.respond(info, format('%s | %s %s (%s) | http://%s/ticket/%s',
+                                       line[1],
+                                       line[6],
+                                       line[11],
+                                       line[8],
+                                       info.plugin.options.host,
+                                       id));
+        }
+      });
+    });
+  }
+};
---
+++ plugins/trac/index.js
@@ -0,0 +1,27 @@
+var commands = require('./cmd');
+
+function parseLine(from, to, msg) {
+  if (msg[0] === '!' && msg.length > 1) {
+    var sp = msg.split(' ', 2)
+      , info = {
+          from: from
+        , to: to
+        , msg: msg
+        , cmdstr: sp[0].substr(1)
+        , rest: sp[1]
+        , bot: plugin.bot
+        , plugin: plugin
+        }
+      ;
+
+    commands.run(info);
+  }
+}
+
+var plugin = module.exports = {
+  listeners: {
+    message: parseLine
+  }
+, reload: ['./cmd'].map(require.resolve)
+, commands: ['!trac']
+};
